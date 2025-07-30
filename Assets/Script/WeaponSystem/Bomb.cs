419ba03dfeed3042809b80146ed9674d323125a4
yec1oud <lyunyi2004@gmail.com>
2025-07-31 01:14:24 +0800

    lyy_level3

---
+++ Assets/Script/WeaponSystem/Bomb.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float bombTimer = 3f;
+    float countDown;
+
+    bool hasExploded = false;
+
+    //public float giveDamage = 120f;
+    //public float radius = 10f;
+
+    //public GameObject explosionEffect;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        countDown = bombTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -= Time.deltaTime;
+
+        if (countDown <= 0 && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+    void Explode()
+    {
+        //showEffect
+        //Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        //get nearby objects
+        //Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        /*
+        foreach (Collider nearbyObject in colliders)
+        {
+            //damage
+            Object obj = nearbyObject.GetComponent<Object>();//find the nearby object that has Object scripts
+
+            if (obj != null)
+            {
+                obj.objectHitDamage(giveDamage);
+            }
+        }
+        */
+        Debug.Log("Explore!");
+
+        Destroy(gameObject);
+    }
+}
