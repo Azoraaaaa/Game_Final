6f34e3e17b357c2c3b6482664bcf9d3204921e60
yec1oud <lyunyi2004@gmail.com>
2025-07-31 16:26:28 +0800

    magic

---
+++ Assets/Script/WeaponSystem/MagicAttack.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class MagicAttack : MonoBehaviour
+{
+    public float throwForce = 10f;
+    public Transform magicArea;
+    public GameObject magicPrefab;
+    public Animator anim;
+    bool nextThrow = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && nextThrow)
+        {
+            StartCoroutine(MagicAnimation());
+        }
+    }
+    void ThrowMagic()
+    {
+        GameObject magic = Instantiate(magicPrefab, magicArea.transform.position, magicArea.transform.rotation);
+        Rigidbody rb = magic.GetComponent<Rigidbody>();
+        rb.AddForce(magicArea.transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+    IEnumerator MagicAnimation()
+    {
+        nextThrow = false;
+        anim.SetBool("MagicInAir", true);
+        yield return new WaitForSeconds(0.5f);
+        ThrowMagic();
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("MagicInAir", false);
+        nextThrow = true;
+    }
+}
