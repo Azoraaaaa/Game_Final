dfc2142d56e8bcd5fdf320dab4e1d81ea1cb3c22
LYU WANYUN <152700249+Azoraaaaa@users.noreply.github.com>
2025-08-02 07:38:04 +0800

    import asset

--- Assets/Script/QuestSystem/PanDing/AreaManager.cs
+++ Assets/Script/QuestSystem/PanDing/AreaManager.cs
@@ -5,7 +5,12 @@ public class AreaManager : MonoBehaviour
 {
     public AreaZone[] zones; // 手动拖入四个区域
     public PlayableDirector bossUnlockTimeline;
+    public GameObject bossCanvas;
 
+    void Start()
+    {
+        bossCanvas.SetActive(false);
+    }
     public void CheckAllZones()
     {
         foreach (var zone in zones)
@@ -17,6 +22,7 @@ public class AreaManager : MonoBehaviour
         // 所有区域都正确，播放Timeline
         if (bossUnlockTimeline != null && bossUnlockTimeline.state != PlayState.Playing)
         {
+            bossCanvas.SetActive(true); 
             bossUnlockTimeline.Play();
         }
     }
